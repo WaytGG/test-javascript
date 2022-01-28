174dc85930c0a155efcface441653a823d586398
Yevhenii Chacha <01flax01@gmail.com>
2022-01-28 16:50:27 +0200

    Class work, task

---
+++ assets/js/tasks.js
@@ -0,0 +1,114 @@
+// Task#1
+
+// function sayHello() {
+//   let userName = prompt("Как Вас зовут?");
+
+//   return alert("Привет" + " " + userName);
+// }
+// sayHello();
+
+
+
+//Task#2
+
+// let  questionUser= prompt("Wanna water?");
+
+// function question(notEmpty) {
+
+//   if (notEmpty === "") {
+//     return false;
+//   }
+
+//   else {
+//     return true;
+//   }
+// }
+
+// let result = question(questionUser);
+
+// console.log(result);
+
+
+
+//Task#3
+
+// let priceProduct = prompt("Цена продукта?");
+
+// let amount = prompt("Количество");
+
+// function price (finalPrice) {
+//   finalPrice = Number(priceProduct * amount);
+
+//   return finalPrice;
+// }
+
+// let result = price(price,amount)
+
+// alert(result);
+
+
+
+//Task#4
+
+// let priceAmount = prompt("Сумма заказа");
+
+// function price() {
+//   let money = Number(300);
+
+//   if (priceAmount > money) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
+
+// let result = price();
+
+// alert(result);
+
+
+
+//Task#5
+
+// let qustionStorey = prompt("Сколько этажей?");
+
+// function amountStorey() {
+//   let answerStorey = Number(qustionStorey * 4);
+//   return answerStorey;
+// }
+
+// let result = amountStorey()
+
+// alert(result);
+
+
+
+//Task#6
+
+// let questionNumber = prompt("Введите число");
+
+// function numberRange(numberAnwer) {
+//   if (Number(numberAnwer) >= 10) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
+
+// let result = numberRange(questionNumber);
+
+// alert(result);
+
+
+//Task#6*
+
+let modulQuestion = prompt("Введите число");
+
+function modulReturn () {
+  Math.abs();
+  return modulReturn;
+}
+
+let result = modulReturn(modulQuestion);
+
+console.log(result);
