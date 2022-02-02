ec3cad106bd4d495ad72aa905e3adf1fc6cf8bfa
Wayt <01flax01@gmail.com>
2022-02-02 12:14:17 +0200

    Update tasks

--- assets/js/jsloops.js
+++ assets/js/jsloops.js
@@ -2,81 +2,94 @@
 
 /* for (let i = 1; i <= 10; i++) {
     console.log(i);
-} */
+}  */
+
 
 
 // 1.2) * вывести в консоль числа от -1 до -10 включительно
-/* 
-for (let i = -1; i >= -10; i--) {
+
+/* for (let i = -1; i >= -10; i--) {
   console.log(i);
-} */
+}  */
+
 
 
 // 2) посчитать результат умножения чисел от 1 до 5
- /*
- let result = 1;
 
- for (let i = 1; i < 6; i++) {
-   result = result *i;
-   console.log(result);
- }
-  
- */
+/* let num = 1;
+ 
+for (let i = 1; i < 6; i++) {
+  num *= i;
+  console.log(num);
+}  */
+
 
 
 // 3) вывести в консоль таблицу умножения на 7
 // пример ('7 * ' + n + ' = ' + res)
 
-/*
- for (let i = 1; i <= 9; i++) {
-   const seven = 7;
-   console.log(seven * i);
- }
-  
- */
+/* const num = 7;
+
+for (let i = 1; i <= 10; i++) {
+  const result = num * i;
+  console.log(result);
+}  */
+
 
 
 // 4) посчитать сумму нечетных чисел от 1 до 15
-/* 
-let sum = 0;
+  
+/* let sum = 0;
+  
 for (let i = 1; i <= 15; i++){
   if (i % 2 === 1) {
     sum += i;
+  }
 }
-console.log(sum);
- */
+console.log(sum); */
+
+
 
 // 5.1) циклом добавить 7 звездочек в строку
 
-/* let str = 'Stars'
-for (let i = 1; i <= 7; i+=2) {
+/* let str = '';
+
+for (let i = 1; i <= 7; i++) {
   str += '*';
-  console.log(str);
-} */
+} 
+console.log(str); */
+
 
 
 // 5.2) * кол-во звездочек определяет пользователь
+/* 
+let userStars = prompt('Введите *');
 
-/* let str = 'Stars'
-for (let i = 1; i <= numberOfStars; i++) {
+function stars (str) {
+  for (let i = 1; i <= str; ) {
   str += '*';
-  console.log(str);
-} */
+  }
+  return str;
+}
+
+result = stars(userStars);
+console.log(result); */
+
+
 
 // 6) * 2 цикла которые добавят звездочки и "\n" в переменную и составят квадрат из звездочек
 
 
 
 // 7) * спрашивать пользователя определенное число до тех пор пока он его не отгадает
- 
-/* const num = 7;
-let a = prompt('Введите число');
+  
 
-function myPromptNumber(x) {
-    while(a != x)
-        alert('Wrong!');
-    alert('Bingo!');
-    return Number(a);
-}
-y = myPromptNumber(num);
-console.log(y); */
+/* while (true) {
+  const num = prompt('Угадайте число от 0 до 10');
+  
+  if (num === 7) {
+    alert('Верно!');
+    break;
+  } 
+  else alert('Нет, введите еще раз');
+} */
