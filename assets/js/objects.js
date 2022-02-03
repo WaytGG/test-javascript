6e777e6e44bad5a0856044bb66626c0604580dd5
Yevhenii Chacha <01flax01@gmail.com>
2022-02-03 13:57:06 +0200

    Remove task car

--- assets/js/objects.js
+++ assets/js/objects.js
@@ -6,10 +6,3 @@
 result = resultNumber(object, 'randNumber');
 
 console.log(result) */
-
-const car = {
-  speed: 0,
-  moveforward: function(){return this.speed = 50},
-  moveBack: function() {return this.speed = 20},
-  stop: function() {return this.speed = 0} 
-}
