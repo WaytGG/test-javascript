d226a5320c550f60ee6dbae3712d08f5d249a6f5
Yevhenii Chacha <01flax01@gmail.com>
2022-02-03 14:19:35 +0200

    Add tasks

---
+++ assets/js/practice-patterns.js
@@ -0,0 +1,39 @@
+// 1. Составить строку из слов и переменных `Я живу в ${country}`, вывести в консоль
+
+/* const country = 'Ukraine';
+
+console.log(`Я живу в ${country}`);
+ */
+
+
+//2. Составить строку из переменных с названиями firstname и id в формате Ivan_39, вывести в консоль
+
+/* let firstname = 'Ivan';
+
+let id = '39';
+
+console.log(`${firstname}_${id}`); */
+
+
+//3. Составить строки из переменных (названия переменных придумайте), значения переменных:
+/* London, NewYork, Minsk,
+Belorussia, USA, GreatBritain
+Формат: `${} is a capital of ${}`
+ */
+/* 
+const gb = 'GreatBritain';
+const lond = 'London';
+
+console.log(`${lond} is a capital of ${gb}`);
+
+
+const bel = 'Belorussia';
+const min = 'Minsk';
+
+console.log(`${min} is a capital of ${bel}`);
+
+
+const usa = 'USA';
+const was = 'Washington'
+
+console.log(`${was} is a capital of ${usa}`); */
