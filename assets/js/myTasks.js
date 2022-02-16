3224185d9b23a6115c71fca8f6f5160e262ae22a
Wayt <01flax01@gmail.com>
2022-02-16 10:44:37 +0200

    rename mytasks

---
+++ assets/js/myTasks.js
@@ -0,0 +1,40 @@
+//Проверка логин/пасс
+
+/* const useruQestion = prompt('Login');
+
+if (useruQestion === 'Wayt') {
+  let pass = prompt('Pass');
+  if (pass === 'qwe') {
+    alert('Hello, Wayt!')
+  }else if (pass === '' || pass === null){
+    alert('Cancel')
+  } else alert('Wrong pass, try again');
+}else if(useruQestion === '' || useruQestion === null) {
+  alert('Cancel :(')
+}else alert('Who are you?') */
+
+///////////////////////////////////////////////////////////
+
+//Четные числа выводит
+/* for (let i = 2; i <= 10; i++) {
+  if (i % 2 === 0){
+    console.log(i);
+  }
+} */
+
+
+//НЕчетные числа выводит
+/* for (let i = 0; i <= 10; i++) {
+  if (i % 2 === 1){
+    console.log(i);
+  }
+} */
+
+
+////////////////////////////////////////////////////////////
+/* let userNum;
+
+do {
+  userNum = +prompt('Введите число больше 100');
+} while (userNum <= 100 && userNum);
+ */
